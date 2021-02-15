0a300e25c3ecc11a848ccf4d7485934b3bb996d1
kla04 <fiw56789@gmail.com>
2021-02-15 20:33:47 +0700

    Add NodeJS

---
+++ Lab5-NodeJS/index.js
@@ -0,0 +1,111 @@
+// var http = require('http');
+// var server = http.createServer(function(req, res){
+//    res.writeHead(200, {'Content-type': 'text/plain'});
+//    res.end('Hello world\n');
+// });
+
+// server.listen(8000);
+// console.log('Server is ready!');
+
+// const express = require('express');
+// const app = express();
+
+// app.get('/', function(req, res){
+//    res.send('<html><body><h1>Hello world</h1></body></html>')
+// });
+
+//qurey string
+// app.get('/greeting', (req, res) => {
+//    let greetText = req.query.str1 + " " + req.query.str2
+//    res.write(`<html><h1 style="align:center;">${greetText}</h1></body></html>`)
+//    res.end()
+// })
+
+//param
+// app.get('/greeting/:str1/:str2/:str3', (req, res) => {
+//    console.log(req)
+//    let greetText = req.params.str1 + " " + req.params.str2 + " " + req.params.str3
+//    res.send(`<html><body><h1 style="align:center;">Hey: ${greetText}</h1></body></html>`)
+//  })
+
+// app.listen(8000);
+
+
+//body-parser
+
+// const express = require('express'),
+//    app = express(),
+//    bodyParser = require('body-parser');
+
+// const urlencodedParser = bodyParser.urlencoded({ extended: false });
+// app.use(express.static(__dirname + '/public'));
+
+// app.post('/add', urlencodedParser, function(req, res){
+//    let result = parseInt(req.body.a) + parseInt(req.body.b);
+//    res.send('Result = ' + result);
+// });
+
+// app.listen(8000);
+
+//cockie parser
+// const express = require('express')
+// const app = express()
+// const cookieParser = require('cookie-parser')
+// app.use(cookieParser('keyboard cat')) //‘keyboard cat’ is a secret key to sign cookie (prevent cookie tamper)
+// app.get('/ck_get', function(req, res) {
+//    res.send(req.cookies)
+// })
+
+// app.get('/ck_set', function(req, res) {
+//    res.cookie('a', 10)
+//    res.send('ok')
+// })
+
+// app.listen(8000)
+
+//session
+
+// const express = require('express')
+// const app = express()
+// const session = require('express-session')
+
+//                          // sign cookie (for a session)
+// app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 },
+//    resave : false, saveUninitialized: false }))
+//    // resave => Forces the session to be saved back to the session store, even if the session was never modified 
+//    // saveUninitialized => the cookie will not be set on a response with an uninitialized session
+
+// app.use(function(req, res, next) {
+//    var sess = req.session
+//    if (sess.views) {
+//        sess.views++
+//    } else {
+//        sess.views = 1
+//    }
+//    console.log(sess.views)
+//    next();
+// })
+// app.get('/',function(req, res) {
+//    res.send('count =' + req.session.views)
+// })
+// app.listen(8000)
+ 
+//Sample API
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const PORT = 8000
+ 
+let tasks =  [
+      { id: 1, name: 'John', age: 30 },
+      { id: 2, name: 'Maew', age: 25 }]
+ 
+app.use(cors())
+ 
+app.get('/', (req,res) => {
+   res.json(tasks)
+})
+ 
+app.listen(PORT, () => console.log(`listen at ${PORT}`))
+
+//npm i -g nodemon
